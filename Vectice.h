9eac4ff8e37a8ba7ca221414ae33600c02c49c8e
machard <pascal.machard@gmail.com>
2015-12-23 16:45:55 +0100

    Update Vectice.h

--- Vectice.h
+++ Vectice.h
@@ -1,11 +1,25 @@
 #pragma once
 
 namespace prj {
-	namespace math {
-
-class Vectice
-{
-};
-
-}
+	namespace modeller {
+			
+		template <typename T> class Vectice : Vector3D<T>
+		{
+		private:
+			long m_Id;
+			static long m_InternalId = 0;
+		public:
+			Vectice<T>(void) : Vector3D<T> ()
+			{
+				m_Id = m_InternalId++;
+			}
+			
+			Vectice<T>(const Vectice<T> & paramVertice) : Vector3D<T>(paramVertice)
+			{
+				m_Id = m_InternalId++;
+			}
+			
+			inline long GetId(void) { return m_Id;};
+		};
+	}
 }
