0776cd2ab11b1a9cfac1fce90d9de681ab17cfe5
machard <pascal.machard@gmail.com>
2016-02-24 17:12:43 +0100

    Update Edge.h

--- Edge.h
+++ Edge.h
@@ -1,38 +1,107 @@
 #pragma once
 
+#include "stdafx.h"
+
+#include <ostream>
+#include <map>
+#include <string>
+
+#include "Vertice.h"
+
+using namespace prj::math;
+using namespace std;
+
 namespace prj {
 	namespace modeller {
-			
-		template <typename T> class Edge
+
+		class Edge
 		{
 		private:
+			static long m_InternalId;
+			static map<long, Edge*> m_mapIdToEdge;
+
 			long m_Id;
-			static long m_InternalId = 0;
-			Vectice m_Va;
-			Vectice m_Vb;
+			long m_Va;
+			long m_Vb;
+			string m_Label;
+
 		public:
-			Edge<T>(void) : Edge<T> ()
+			static long IncId(Edge * pEdge)
 			{
-				m_Id = m_InternalId++;
+				m_InternalId++;
+				m_mapIdToEdge.insert(pair<long, Edge*>(m_InternalId, pEdge));
+				return m_InternalId;
+			};
+
+			static long Id(void) { return m_InternalId; };
+
+			static const Edge & GetWithId(long id)
+			{
+				map<long, Edge*>::iterator itFind = m_mapIdToEdge.find(id);
+				return *(itFind->second);
 			}
-			
-			Edge<T>(const Edge<T> & paramEdge) 
+
+			Edge(void) 
 			{
-				m_Id = m_InternalId++;
+				m_Label = "";
+				m_Id = IncId(this);
+			}
+
+			Edge(long vaId, long vbId,string label)
+			{
+				m_Va = vaId;
+				m_Vb = vbId;
+				m_Label = label;
+				m_Id = IncId(this);
+			}
+
+			Edge(const Edge & paramEdge)
+			{
 				m_Va = paramEdge.m_Va;
 				m_Va = paramEdge.m_Vb;
+				m_Label = paramEdge.m_Label;
+				m_Id = IncId(this);
 			}
-			
-			inline long GetId(void) { return m_Id;};
-			
-			inline Vectice & GetVecticeA(void) { return m_Va;};
-			inline Vectice & GetVecticeB(void) { return m_Vb;};
-			inline const Vectice & GetVecticeA(void) const { return m_Va;};
-			inline const Vectice & GetVecticeB(void) const { return m_Vb;};
-			inline void SetVecticeA(const Vectice & paramVectice)  { m_Va = paramVectice;};
-			inline void SetVecticeB(const Vectice & paramVectice)  { m_Vb = paramVectice;};
 
-			Vectice m_Vb;
+			inline long GetId(void) const 
+			{ 
+				return m_Id; 
+			};
+
+			inline const string & GetLabel(void) const
+			{
+				return m_Label;
+			};
+
+			inline const Vertice & GetA(void) const 
+			{ 
+				return Vertice::GetWithId(m_Va); 
+			};
+
+			inline const Vertice & GetB(void) const 
+			{ 
+				return Vertice::GetWithId(m_Vb); 
+			};
+
+			inline Vertice & GetA(void) 
+			{ 
+				return Vertice::GetWithId(m_Va); 
+			};
+
+			inline Vertice & GetB(void) 
+			{ 
+				return Vertice::GetWithId(m_Vb); 
+			};
+
+			friend std::ostream & operator<< (std::ostream & paramOutput, const Edge & paramEdge)
+			{
+				paramOutput << "(Id:" << paramEdge.GetId() << ";";
+				paramOutput << "(label:(" << paramEdge.GetLabel() << ");";
+				paramOutput << "(A:(" << paramEdge.GetA() << ");B:(" << paramEdge.GetB() << ");";
+				paramOutput << ")";
+				return paramOutput;
+			}
+
 		};
 	}
 }
