7a3a28718f7c42545487e9098287e182d66c1d1d
machard <pascal.machard@gmail.com>
2016-02-24 17:13:00 +0100

    Update Vectice.h

--- Vectice.h
+++ Vectice.h
@@ -1,25 +1,77 @@
 #pragma once
+#include "stdafx.h"
+
+#include <ostream>
+#include <string>
+#include <map>
+
+#include "VectorXd.h"
+
+using namespace prj::math;
+using namespace std;
 
 namespace prj {
 	namespace modeller {
-			
-		template <typename T> class Vectice : Vector3D<T>
+
+		class Vertice : public Vector3d<double>
 		{
 		private:
+			static long m_InternalId;
+			static map<long, Vertice*> m_mapIdToVertice;
+
 			long m_Id;
-			static long m_InternalId = 0;
+			string m_Label;
 		public:
-			Vectice<T>(void) : Vector3D<T> ()
+
+			static long IncId(Vertice * pVertice)
 			{
-				m_Id = m_InternalId++;
-			}
-			
-			Vectice<T>(const Vectice<T> & paramVertice) : Vector3D<T>(paramVertice)
+				m_InternalId++;
+				m_mapIdToVertice.insert(pair<long, Vertice*>(m_InternalId, pVertice));
+				return m_InternalId;
+			};
+			static long Id(void) { return m_InternalId; };
+
+			static Vertice & GetWithId(long id)
 			{
-				m_Id = m_InternalId++;
+				map<long, Vertice*>::iterator itFind = m_mapIdToVertice.find(id);
+				return *(itFind->second);
 			}
-			
-			inline long GetId(void) { return m_Id;};
+
+			Vertice(void) : Vector3d<double>()
+			{
+				m_Id = IncId(this);
+			}
+
+			Vertice(const Vertice & paramVertice) : Vector3d<double>(paramVertice)
+			{
+				m_Label = paramVertice.m_Label;
+				m_Id = IncId(this);
+			}
+
+			Vertice(const Vector3d & paramVector3D,const string label) : Vector3d<double>(paramVector3D)
+			{
+				m_Label = label;
+				m_Id = IncId(this);
+			}
+
+			inline long GetId(void) const
+			{ 
+				return m_Id; 
+			};
+
+			inline const string & GetLabel(void) const
+			{
+				return m_Label;
+			};
+
+			friend std::ostream & operator<< (std::ostream & paramOutput, const Vertice & paramVectice)
+			{
+				paramOutput << "(Id:" << paramVectice.GetId() << ";";
+				paramOutput << "(label:(" << paramVectice.GetLabel() << ");";
+				paramOutput << "(Vector:" << (const Vector3d<double> &)paramVectice;
+				paramOutput << ")";
+				return paramOutput;
+			}
 		};
 	}
 }
