6d00ebb9a89318631a3673cee7a0c137337d6d1e
machard <pascal.machard@gmail.com>
2016-02-24 17:14:25 +0100

    Create CaoMaker.h

---
+++ CaoMaker.h
@@ -0,0 +1,26 @@
+
+#pragma once
+#include "stdafx.h"
+#include "Vertice.h"
+#include <ostream>
+#include <string>
+
+using namespace prj::math;
+using namespace prj::modeller;
+using namespace std;
+
+namespace prj {
+	namespace cao {
+
+		class CaoMaker
+		{
+		public :
+			static void MakeBox(string label, Vertice origine, double wide, double length, double height)
+			{
+
+			}
+		};
+	}
+}
+		
+		
