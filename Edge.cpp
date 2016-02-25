d1324ffcbcbe32acfd07116e6dddf0d103b48877
machard <pascal.machard@gmail.com>
2016-02-25 12:11:42 +0100

    Update Edge.cpp

--- Edge.cpp
+++ Edge.cpp
@@ -1,9 +1,12 @@
 #include "stdafx.h"
 #include <map>
+#include <list>
+#include <string>
 
 #include "Edge.h"
 
 using namespace prj::modeller;
 
-long Edge::m_InternalId = 0;
+long ::Edge::m_InternalId = 0;
+
 map<long, Edge*> Edge::m_mapIdToEdge;
